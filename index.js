61a080fa84c4caa510e9d578cff3d3683f42dcbd
Yeasin <ar9485727@gmail.com>
2024-06-04 14:14:08 +0600

    user put req

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
-const jwt = require('jsonwebtoken')
-const cookieParser = require('cookie-parser')
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
@@ -22,13 +22,8 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-
-
-
-
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
-
 
 const uri = `mongodb+srv://${username}:${password}@cluster0.fvuhgjn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -38,56 +33,84 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
 
-    const db = client.db('m-12')
-    const bioDatasCollection = db.collection('bioDatas')
-    const successStoriesCollection = db.collection('successStories')
+    const db = client.db("m-12");
+    const bioDatasCollection = db.collection("bioDatas");
+    const successStoriesCollection = db.collection("successStories");
+    const userCollection = db.collection("user");
 
     // get all the bioDatas
-    app.get('/bioDatas', async (req, res) => {
+    app.get("/bioDatas", async (req, res) => {
       const bioDatas = await bioDatasCollection.find().toArray();
       res.send(bioDatas);
-    })
+    });
 
     // get req for single bioData
-    app.get('/bioDatas/:id', async (req, res) => {
+    app.get("/bioDatas/:id", async (req, res) => {
       const id = req.params.id;
-      const bioData = await bioDatasCollection.findOne({ _id: new ObjectId(id) });
+      const bioData = await bioDatasCollection.findOne({
+        _id: new ObjectId(id),
+      });
       res.send(bioData);
-    })
+    });
 
-    app.get('/stats', async (req, res) => {
+    app.get("/stats", async (req, res) => {
       try {
         const totalBioDatas = await bioDatasCollection.countDocuments();
-        const totalGirls = await bioDatasCollection.countDocuments({ biodataType: 'Female' });
-        const totalBoys = await bioDatasCollection.countDocuments({ biodataType: 'Male' });
-        const totalMarriages = await bioDatasCollection.countDocuments({ marriageCompleted: true });
+        const totalGirls = await bioDatasCollection.countDocuments({
+          biodataType: "Female",
+        });
+        const totalBoys = await bioDatasCollection.countDocuments({
+          biodataType: "Male",
+        });
+        const totalMarriages = await bioDatasCollection.countDocuments({
+          marriageCompleted: true,
+        });
         res.send({ totalBioDatas, totalGirls, totalBoys, totalMarriages });
       } catch (error) {
-        res.status(500).send({ error: 'Failed to fetch stats' });
+        res.status(500).send({ error: "Failed to fetch stats" });
       }
     });
 
     // success story collection
-    app.get('/successStories', async (req, res) => {
+    app.get("/successStories", async (req, res) => {
       const successStories = await successStoriesCollection.find().toArray();
       res.send(successStories);
-    })
+    });
+
+    // save user
+    app.put("/user", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const isExist = await userCollection.findOne(query);
+
+      if (isExist) {
+        return res.status(400).send({ error: "User already exist" });
+      }
+
+      const option = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(query, updateDoc, option);
+      res.send(result);
+    });
 
     // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
